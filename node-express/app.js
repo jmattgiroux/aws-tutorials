5f67680a5540f453ec0a094a03a7d22dc67bd9af
jmattgiroux <jmattgiroux@gmail.com>
2023-02-21 15:08:53 -0600

    Added route and view for My Programming Log

--- node-express/app.js
+++ node-express/app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var programming = require('./routes/programming');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -8,6 +9,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.get('/programming', programming.index);
+app.post('/add_programming', programming.add_programming);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ node-express/routes/programming.js
@@ -0,0 +1,7 @@
+exports.index = function(req, res) {
+  res.render('programming', { title: 'My Programming Log' });
+}
+
+exports.add_programming = function(req, res) {
+
+}
