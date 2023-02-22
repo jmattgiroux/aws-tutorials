e9654f4a89a250e3f29d94127da970ab78378a44
jmattgiroux <jmattgiroux@gmail.com>
2023-02-22 13:00:41 -0600

    Added message about CloudFront

--- node-docker-eb/index.js
+++ node-docker-eb/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Now I can update what\'s on EB just by pushing to GitHub!'));
+app.get('/', (req, res) => res.send('Look Ma, I\'m on CloudFront!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
