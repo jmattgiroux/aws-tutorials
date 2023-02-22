126c00aab805dc6682125bb848bb45a889c8a637
jmattgiroux <jmattgiroux@gmail.com>
2023-02-22 10:07:00 -0600

    Added message to index page showing that buddy was involved

--- node-docker-eb/index.js
+++ node-docker-eb/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Docker from Elastic Beanstalk!'));
+app.get('/', (req, res) => res.send('Hello Docker from Elastic Beanstalk and Buddy!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
